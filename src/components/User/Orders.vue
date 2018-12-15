73d20a56b23b7cae5ce9e9a1206398f15a771772
Denis <ddd329@yandex.ru>
2018-12-15 17:49:14 +0700

    Первый коммит

---
+++ src/components/User/Orders.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-card>
+          <v-list subheader two-line>
+            <v-list-tile
+              avatar
+              v-for="order in orders"
+              :key="order.id"
+            >
+              <v-list-tile-action>
+                <v-checkbox
+                  color="success"
+                  :input-value="order.done"
+                  @change="markDone(order)"
+                ></v-checkbox>
+              </v-list-tile-action>
+
+              <v-list-tile-content @click="notifications = !notifications">
+                <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn :to="'/ad/' + order.id" class="primary">Open</v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      orders: [
+        {
+          id: 'erewr',
+          name: 'Denis',
+          phone: '8-913-789-32-56',
+          adId: '123',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import * as fb from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyBD4awijy5cHrWfd9Nh8VRNSzhQ1ve_TmU',
+      authDomain: 'itc-ads-ddd329.firebaseapp.com',
+      databaseURL: 'https://itc-ads-ddd329.firebaseio.com',
+      projectId: 'itc-ads-ddd329',
+      storageBucket: 'itc-ads-ddd329.appspot.com',
+      messagingSenderId: '511455135363'
+    })
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
+  }
+})
